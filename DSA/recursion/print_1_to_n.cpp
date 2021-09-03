d56536904b435283b303a67fe1172f9bf8392240
Keshav Kumar <keshavkumar8122@gmail.com>
2021-09-03 21:50:05 +0530

    3rd sept 21

---
+++ DSA/recursion/print_1_to_n.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+//#include <iostream>
+using namespace std;
+void print(int n){
+    if (n==0)
+    {
+        return;
+    }
+    print(n-1);
+    cout<<n<<" ";
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}

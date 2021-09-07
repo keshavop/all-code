3da2f90276f8a2e4a86876a9e69f98c253711a38
Keshav Kumar <keshavkumar8122@gmail.com>
2021-09-07 20:24:39 +0530

    7th sept 21

--- DSA/Recursion/rope_cutting_problem.cpp
+++ DSA/Recursion/rope_cutting_problem.cpp
@@ -1,34 +1,30 @@
-
-//  code is wrong
-
-#include <bits/stdc++.h> 
-#include<iostream> 
+#include <iostream>
 using namespace std;
 
-int pieces(int n,int a,int b,int c){
-    if (n==0)
-    {
+int maxCuts(int n, int a, int b, int c)
+{
+    if (n == 0)
         return 0;
-    }
-    if (n<0)
-    {
+    if (n <= -1)
         return -1;
-    }
 
-    int res=max(pieces(n-a,a,b,c),pieces(n-b,a,b,c),pieces(n-c,a,b,c));
-    
-    if (res==-1){
+    int res = max(maxCuts(n - a, a, b, c),max(maxCuts(n - b, a, b, c),maxCuts(n - c, a, b, c)));
+
+    if (res == -1)
         return -1;
-    }
-    else{
-        return res+1;
-    }
+
+    return res + 1;
 }
-
 int main()
 {
-    int n,a,b,c;
-    cin>>n>>a>>b>>c;
-    cout<<pieces(n,a,b,c);
+
+    // int n = 5, a = 2, b = 1, c = 5;
+    int n;
+    cin>>n;
+    int a,b,c;
+    cin>>a>>b>>c;
+
+    cout << maxCuts(n, a, b, c);
+
     return 0;
 }

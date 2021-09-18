393f8ac99a4202d0ee2a0588ac6568faf0bce17d
Keshav Kumar <keshavkumar8122@gmail.com>
2021-09-18 20:40:14 +0530

    18th sept 21

---
+++ DSA/11 Linked List/04_insert_at_the_end.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+
+
+
+node *insertEnd(node *head, int d)
+{
+    node *temp = new node(d);
+    if (head == NULL)
+        return temp;
+
+    node *curr = head;
+    while (curr->next != NULL)
+    {
+        curr=curr->next;
+    }
+    curr->next = temp;
+}
+
+void print(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+
+int main()
+{
+    node *head = NULL;
+    head = insertEnd(head, 10);
+    head = insertEnd(head, 20);
+    head = insertEnd(head, 30);
+
+    print(head);
+    return 0;
+}

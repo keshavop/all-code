a63f32962b1fccbd1ad54fb92cb42d518f2518a0
Keshav Kumar <keshavkumar8122@gmail.com>
2021-09-19 01:01:19 +0530

    q2

---
+++ python_class/if-else/q2.py
@@ -0,0 +1,9 @@
+# Write a program that asks for two numbers. If the sum of the numbers is greater than 100, print "That is a big number" and terminate the program.
+
+a=int(input())
+b=int(input())
+
+sum=a+b
+
+if sum>100:
+    print(sum,"This is a big number")

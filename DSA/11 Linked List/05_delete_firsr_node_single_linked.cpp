cdf4b622754fe6cd2a64a59d5f9085983a01fdcb
Keshav Kumar <keshavkumar8122@gmail.com>
2021-09-19 20:06:37 +0530

    19th sept

--- DSA/11 Linked List/05_delete_firsr_node_single_linked.cpp
+++ DSA/11 Linked List/05_delete_firsr_node_single_linked.cpp
@@ -2,30 +2,44 @@
 using namespace std;
 
 class node{
-    public:
+public:
     int data;
-    node*next;
-    node(int x){
-        data=x;
-        next=NULL;
+    node *next;
+    node(int x)
+    {
+        data = x;
+        next = NULL;
     }
 };
 
-void print(node*head){
-    while (head!=NULL)
+void print(node *head){
+    while (head != NULL)
     {
-        cout<<head->data<<" ";
-        head=head->next;
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+node *deleteNode(node *head){
+    if (head == NULL){
+        return NULL;
+    }
+    else{
+        node *temp = head->next;
+        delete head;
+        return temp;
     }
 }
 
-node*deleteNode(node*head){
-    
-}
+int main(){
+    node *head = new node(10);
+    head->next = new node(20);
+    head->next->next = new node(30);
+    head->next->next->next = new node(40);
 
-
-int main()
-{
-    node*head=NULL;
+    print(head);
+    head = deleteNode(head);
+    print(head);
     return 0;
 }

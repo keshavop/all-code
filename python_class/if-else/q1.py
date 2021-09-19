f27f971232ceb1a4c43698755ce8a1a95fd00816
Keshav Kumar <keshavkumar8122@gmail.com>
2021-09-20 00:54:12 +0530

    q1

--- python_class/if-else/q1.py
+++ python_class/if-else/q1.py
@@ -1,1 +1,12 @@
 # Write a password guessing program to keep track of how many times the user has entered the password wrong. If it is more than 3 times, print "You have been denied access." and terminate the program. If the password is correct, print "You have successfully logged in." and terminate the program.
+
+password = "qwerty"
+
+count = 3
+for i in range(count):
+    inputPass=input()
+    if inputPass ==password:
+        print("Access Granted")
+        break
+    else:
+        print("Access Denied")

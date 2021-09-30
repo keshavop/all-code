c0c8094a79a3c2d7e28bbce1d9c3f38360c72582
Keshav Kumar <keshavkumar8122@gmail.com>
2021-09-30 15:13:31 +0530

    Update q6.py

--- python_class/if-else/q6.py
+++ python_class/if-else/q6.py
@@ -2,10 +2,10 @@
 
 for i in range(1,51):
     if(i%15==0):
-        print("FizzBuzz")
+        print("FizzBuzz",end=", ")
     elif(i%5==0):
-        print("Buzz")
+        print("Buzz",end=", ")
     elif(i%3==0):
-        print("Fizz")
+        print("Fizz",end=", ")
     else:
-        print(i)
+        print(i,end=", ")

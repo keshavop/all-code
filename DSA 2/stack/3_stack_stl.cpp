07fee779d68ae79119a47a72ca0da7989ba7c628
Keshav Kumar <keshavkumar8122@gmail.com>
2021-10-18 22:17:04 +0530

    stack

---
+++ DSA 2/stack/3_stack_stl.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    stack<int> s;
+
+    for (int i = 1; i <= 6; i++)
+    {
+        s.push(i);
+    }
+
+    while (!s.empty())
+    {
+        cout << s.top() << " ,";
+        s.pop();
+    }
+
+    return 0;
+}

70008b8ebdc57bfa838acad9c5dab7bd7022ab36
Keshav Kumar <keshavkumar8122@gmail.com>
2021-12-16 00:41:13 +0530

    tree 15.12.21

---
+++ DSA/Tree/inorder_binary_tree_traversal.cpp
@@ -0,0 +1,36 @@
+#include <iostream> //  L N R
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void inorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->right = new Node(5);
+
+    inorder(root);
+    return 0;
+}
---
+++ DSA/Tree/postorder_binary_tree_traversal.cpp
@@ -0,0 +1,36 @@
+#include <iostream> //  L N R
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void postorder(Node *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    struct Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->right = new Node(5);
+
+    postorder(root);
+    return 0;
+}

775bb1d5ced7fdff1934734b4619f3403f6066ce
Keshav Kumar <keshavkumar8122@gmail.com>
2022-01-01 23:00:51 +0530

    codechef learning path 2

--- Codechef/learning_camp_2021/Enormous_Input_Test.cpp
+++ Codechef/learning_camp_2021/Enormous_Input_Test.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 int main()
 {
+    int n;
+    cin>>n;
+    int divide;
+    cin>>divide;
+    int count=0,num;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>num;
+        if (num%divide==0)
+        {
+            count++;
+        }
+    }
+    
+    cout<<count;
 
     return 0;
 }

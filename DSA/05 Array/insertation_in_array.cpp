3ec2d831142b81832e7286352e25daf5bda58b20
Keshav Kumar <keshavkumar8122@gmail.com>
2022-01-05 00:59:33 +0530

    day 3

--- DSA/05 Array/insertation_in_array.cpp
+++ DSA/05 Array/insertation_in_array.cpp
@@ -1,36 +1,37 @@
-#include <bits/stdc++.h>
-//#include <iostream>
+// #include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-int insert(int arr[],int n,int x,int pos){
+int insert(int arr[], int n, int x, int pos)
+{
     int cap;
-    if (n==cap)
+    if (n == cap)
     {
         return n;
     }
-    int idx=pos-1;
-    for (int i = n-1; i >=idx; i--)
+    int idx = pos - 1;
+    for (int i = n - 1; i >= idx; i--)
     {
-        arr[i+1]=arr[i];
+        arr[i + 1] = arr[i];
     }
-    arr[idx]=x;
-    return n+1;
+    arr[idx] = x;
+    return n + 1;
 }
 
 int main()
 {
-    int n,x,pos;
-    cin>>n>>x>>pos;
+    int n, x, pos;
+    cin >> n >> x >> pos;
     int arr[n];
     for (int i = 0; i < n; i++)
     {
-        cin>>arr[i];
+        cin >> arr[i];
     }
-    cout<<insert(arr,n,x,pos)<<endl;
+    cout << insert(arr, n, x, pos) << endl;
     for (int i = 0; i < n; i++)
     {
-        cout<<arr[i]<<" ";
+        cout << arr[i] << " ";
     }
-    
+
     return 0;
 }

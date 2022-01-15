42fc59ee6ab4a2864f3b1de62b6de4275fba5cf9
Keshav Kumar <keshavkumar8122@gmail.com>
2022-01-15 21:05:53 +0530

    day 2

--- DSA 2/array/reverse_an_array.cpp
+++ DSA 2/array/reverse_an_array.cpp
@@ -1,12 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int reverse
+int reverse(int arr[],int n){
+    int low=0;
+    int high=n-1;
+    while(low<high){
+        int temp=arr[low];
+        arr[low]=arr[high];
+        arr[high]=temp;
+        low++;
+        high--;
+    }
+}
 
 int main()
 {
     int n=5;
     int arr[n]={2,4,6,80,10};
-    cout<<reverse(arr);
+    reverse(arr,n);
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
     return 0;
 }

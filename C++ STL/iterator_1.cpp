d48e2804868e3e1f8b8df9f4bdc20c63237952f1
Keshav Kumar <keshavkumar8122@gmail.com>
2022-01-21 21:38:35 +0530

    day2

---
+++ C++ STL/iterator_1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector <int> v ={10,20,30,40,50};
+    vector <int> :: iterator i =v.begin();
+    
+    return 0;
+}

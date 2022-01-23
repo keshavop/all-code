d2c95cfa3e5df59c7d174fe780f92df13de950c7
Keshav Kumar <keshavkumar8122@gmail.com>
2022-01-24 01:07:55 +0530

    fomat the java code

--- JAVA/hello_world.java
+++ JAVA/hello_world.java
@@ -1,4 +1,4 @@
-class hello_world{
+class hello_world {
     public static void main(String[] args) {
         System.out.println("hello!");
     }
--- JAVA/input_in_java.java
+++ JAVA/input_in_java.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 public class input_in_java {
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        int x=s.nextInt();
-        int y=s.nextInt();
-        int sum=x+y;
-        System.out.println("Sum is "+sum);
+        Scanner s = new Scanner(System.in);
+        int x = s.nextInt();
+        int y = s.nextInt();
+        int sum = x + y;
+        System.out.println("Sum is " + sum);
     }
 }

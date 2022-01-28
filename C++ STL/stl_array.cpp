09bb41ae30aae09594a0504fe295477a37ce4274
Keshav Kumar <keshavkumar8122@gmail.com>
2022-01-28 16:31:24 +0530

    array stl

---
+++ C++ STL/stl_array.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    array<int, 5> arr;
+    arr.fill(10);     //{10,10,10,10,10}
+
+    arr.at(index);
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<arr.at(i)<<" ";
+    }
+    
+    return 0;
+}

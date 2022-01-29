9c75c26b2589998de64e5dc6aa4e04d444c904a1
Keshav Kumar <keshavkumar8122@gmail.com>
2022-01-30 01:11:53 +0530

    Create Chef_and_Lockout_Draws.cpp

---
+++ Codechef/Chef_and_Lockout_Draws.cpp
@@ -0,0 +1,16 @@
+// starter 23   26th jan 2022
+// code CHFPROFIT
+
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        
+    }
+    
+    return 0;
+}

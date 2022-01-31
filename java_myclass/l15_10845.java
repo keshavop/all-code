abba4a16b5b895faa61c58ef846e05818d191ab2
Keshav Kumar <keshavkumar8122@gmail.com>
2022-01-31 13:41:04 +0530

    update

--- java_myclass/l15_10845.java
+++
@@ -1,9 +0,0 @@
-import javax.sound.sampled.SourceDataLine;
-
-public class l15_10845 {
-    public static void main(String[] args) {
-        if (args[0].equals("Sunday") || (args[0].equals("Saturday"))) {
-            System.out.println("Holiday");
-        }
-    }
-}

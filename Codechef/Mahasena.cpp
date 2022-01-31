76222f34be5af74501ce2e1a817b27ecc3c0d6ac
Keshav Kumar <keshavkumar8122@gmail.com>
2022-01-31 12:39:11 +0530

    day 2

--- Codechef/Mahasena.cpp
+++ Codechef/Mahasena.cpp
@@ -3,14 +3,35 @@ using namespace std;
 int main()
 {
     int t;
-    cin>>t;
-    int count=0;
-    while (t--)
+    cin >> t;
+    int arr[t];
+
+    for (int i = 0; i < t; i++)
     {
-        int n;
-        cin>>n;
-        
+        cin >> arr[i];
     }
-    
+
+    int e = 0, o = 0;
+    for (int i = 0; i < t; i++)
+    {
+
+        if (arr[i] % 2 == 0)
+        {
+            e++;
+        }
+        else
+        {
+            o++;
+        }
+    }
+
+    if (e > o)
+    {
+        cout << "READY FOR BATTLE";
+    }
+    else
+    {
+        cout << "NOT READY";
+    }
     return 0;
 }

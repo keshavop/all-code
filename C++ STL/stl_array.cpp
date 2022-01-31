b20970ffc98cb015cd25135b7c4761464efceb32
Keshav Kumar <keshavkumar8122@gmail.com>
2022-01-31 20:13:07 +0530

    stl day 1

--- C++ STL/stl_array.cpp
+++ C++ STL/stl_array.cpp
@@ -5,13 +5,21 @@ using namespace std;
 int main()
 {
     array<int, 5> arr;
-    arr.fill(10);     //{10,10,10,10,10}
 
-    arr.at(0);
+    //fill func is used to initialise whole array with 10
+    arr.fill(10); //{10,10,10,10,10}
 
-    for (int i = 0; i < 5; i++)
+    arr.at(0); //pointing at arr[0]
+
+    for (int i = 0; i < 5; i++) //printing array
     {
-        cout<<arr.at(i)<<" ";
+        cout << arr.at(i) << " ";
+    }
+
+    array<int, 5> arr1 = {1, 3, 5, 7, 9};
+    for (auto it : arr1.begin(); it != arr1.end(); it++)
+    {
+        cout << *it << " ";
     }
     
     return 0;
---
+++ C++ STL/stl_rough.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s = "xdujfubudh";
+    for (auto c: s)
+    {
+        cout << c << " ";
+    }
+
+    return 0;
+}

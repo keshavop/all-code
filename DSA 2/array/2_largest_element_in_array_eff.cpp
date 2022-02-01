9f90dfaf05430c70a05386b775c0099210374b7b
Keshav Kumar <keshavkumar8122@gmail.com>
2022-02-02 00:42:22 +0530

    day 1
    dsa day 2

---
+++ DSA 2/array/2_largest_element_in_array_eff.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int largest(int arr[], int n) //time -> n
+{
+    int res = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] > arr[res])
+        {
+            res = i;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    int n = 5;
+    int arr[5] = {1, 8, 9, 7, 6};
+    cout << largest(arr, n);
+    return 0;
+}

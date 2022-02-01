100bbdfd8bd9ff02e043eb65cff15d92f920b51b
Keshav Kumar <keshavkumar8122@gmail.com>
2022-02-01 17:42:21 +0530

    searching in array

---
+++ DSA 2/array/1_searching_in_array.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int search(int arr[], int n, int x)
+{
+    for (int i = 0; i < n; i++)
+    {
+        if (x == arr[i])
+        {
+            cout << "Found";
+        }
+    }
+    // return -1;
+    cout<<"Not found";
+}
+
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    search(arr, n, x);
+
+    return 0;
+}

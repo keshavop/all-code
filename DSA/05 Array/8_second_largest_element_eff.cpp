3ba98de39963995778804e71cf6a04842c3280c3
Keshav Kumar <keshavkumar8122@gmail.com>
2022-02-02 19:41:24 +0530

    dsa 2
    array dsa day 2

--- DSA/05 Array/8_second_largest_element_eff.cpp
+++ DSA/05 Array/8_second_largest_element_eff.cpp
@@ -1,37 +1,31 @@
-#include <iostream>
+#include <bits/stdc++.h> 
+#include<iostream> 
 using namespace std;
-int secondLargest(int arr[], int n)
-{
-    int largest = 0, secondLargest = -1;
-    // finding the largest element in the array
-    for (int i = 1; i < n; i++)
-    {
-        if (arr[i] > arr[largest])
-            largest = i;
-    }
-    // finding the largest element in the array excluding
-    // the largest element calculated above
+
+int secondLargest(int arr[],int n){
+    int res=-1,largest=0;
     for (int i = 0; i < n; i++)
     {
-        if (arr[i] != arr[largest])
-        {
-            // first change the value of second largest
-            // as soon as the next element is found
-            if (secondLargest == -1)
-                secondLargest = i;
-            else if (arr[i] > arr[secondLargest])
-                secondLargest = i;
+        if(arr[i]>arr[largest]){
+            res=largest;
+            largest=i;
+        }
+        else if(arr[i]=arr[largest]){
+            if(res==-1 || arr[i]>arr[res]){
+                res = i;
+            }
         }
     }
-    return secondLargest;
+    return res;
 }
-int main()
-{
-    int arr[] = {10, 12, 20, 4};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int second_Largest = secondLargest(arr, n);
-    if (second_Largest == -1)
-        cout << "Second largest didn't exit\n";
-    else
-        cout << "Second largest : " << arr[second_Largest];
+
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i = 0; i < n; i++){
+        cin>>arr[i];
+    }
+    cout<<secondLargest(arr,n);
+    return 0;
 }

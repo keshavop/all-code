25d3eec22d54a16069bec7f962116590f3e821fd
Keshav Kumar <keshavkumar8122@gmail.com>
2022-02-06 17:22:51 +0530

    java

--- JAVA/input_in_java.java
+++ JAVA/input_in_java.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 public class input_in_java {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int x = s.nextInt();
-        int y = s.nextInt();
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
         int sum = x + y;
         System.out.println("Sum is " + sum);
     }

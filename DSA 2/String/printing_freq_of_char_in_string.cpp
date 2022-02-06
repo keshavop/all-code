99578ca9ad8ec533bd933af2d9bf39ae74ad47aa
Keshav Kumar <keshavkumar8122@gmail.com>
2022-02-07 01:16:52 +0530

    string day 1

--- DSA 2/String/printing_freq_of_char_in_string.cpp
+++ DSA 2/String/printing_freq_of_char_in_string.cpp
@@ -6,6 +6,18 @@ using namespace std;
 int main()
 {
     string str = "keshav is OP";
-    
+    int count[26] = {0};
+    for (int i = 0; i < str.length(); i++)
+    {
+        count[str[i] - 'a']++;
+    }
+    for (int i = 0; i < 26; i++)
+    {
+        if (count[i] > 0)
+        {
+            cout << char(i + 'a') << " " << count[i] << endl;
+        }
+    }
+
     return 0;
 }

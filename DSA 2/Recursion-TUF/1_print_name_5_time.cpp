10c2bdbdc8dba944bf0eb9844dfe18e7b21bae50
Keshav Kumar <keshavkumar8122@gmail.com>
2022-02-21 01:40:34 +0530

    Print name 5 time using recursion

---
+++ DSA 2/Recursion-TUF/1_print_name_5_time.cpp
@@ -0,0 +1,26 @@
+// Print name 5 time using recursion
+
+//Time  -> 0(n)
+//Space -> 0(n)
+
+#include <iostream>
+using namespace std;
+
+int print(int i, int n)
+{
+    if (i > n)
+    {
+        return -1;
+    }
+    cout << "kiki" << endl;
+    i++;
+    print(i, n);
+}
+
+int main()
+{
+    int i = 0;
+    int n = 5;
+    print(i, n);
+    return 0;
+}

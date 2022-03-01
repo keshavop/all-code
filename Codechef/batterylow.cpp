d9ce8fea2c0bf1f16545ce1d853894e4151f8b0c
Keshav Kumar <keshavkumar8122@gmail.com>
2022-03-02 00:04:06 +0530

    cp

---
+++ Codechef/batterylow.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        cin>>n;
+        if(n<=15){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+    }
+    return 0;
+}

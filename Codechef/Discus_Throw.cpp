7fd0c72a4e678f08b46581db49a1fc26e408439f
Keshav Kumar <keshavkumar8122@gmail.com>
2022-03-12 22:29:15 +0530

    cp

---
+++ Codechef/Discus_Throw.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+        // cout<<a<<b<<c;
+        int num=max(a,b,c);
+        // cout<<max_num<<endl;
+    }
+
+    return 0;
+}

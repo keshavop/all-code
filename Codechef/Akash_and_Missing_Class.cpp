681338be4ddea53dca656c187494d4fbb7efc5da
Keshav Kumar <keshavkumar8122@gmail.com>
2022-03-13 22:00:16 +0530

    cp

--- Codechef/Akash_and_Missing_Class.cpp
+++ Codechef/Akash_and_Missing_Class.cpp
@@ -7,7 +7,22 @@ int main()
     cin >> t;
     while (t--)
     {
-        
+        int a;
+        cin >> a;
+        int temp;
+        if (a < 6)
+        {
+            cout << "0" << endl;
+        }
+        else if (a == 6)
+        {
+            cout << "1" << endl;
+        }
+        else
+        {
+            temp = a / 7;
+            cout << temp << endl;
+        }
     }
 
     return 0;

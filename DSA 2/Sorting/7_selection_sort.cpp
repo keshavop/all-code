0c93c68f5ea1a25dcb9ab610e919938640c609f1
Keshav Kumar <keshavkumar8122@gmail.com>
2022-03-19 23:46:12 +0530

    sorting

--- DSA 2/Sorting/7_selection_sort.cpp
+++ DSA 2/Sorting/7_selection_sort.cpp
@@ -1,5 +1,7 @@
 // selection sort
-//
+// find the smallest element and put in first place and proceed on
+// we have half part array sorted and half array sorted
+// Unstable sorting algorithm
 
 // Time -> 0(n^2)
 // Space - > 0(1)
@@ -9,13 +11,26 @@ using namespace std;
 
 void selectionSort(int arr[], int n)
 {
+    for (int i = 0; i < n - 1; i++)
+    {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_index])
+            {
+                swap(arr[min_index], arr[i]); // swapping smallest element
+            }
+        }
+    }
 }
 
 int main()
 {
     int arr[] = {10, 20, 50, 60, 97, 69, 81};
     int n = sizeof(arr) / sizeof(arr[0]);
+
     selectionSort(arr, n);
+
     cout << "After sorting" << endl;
     for (int i = 0; i < n; i++)
     {

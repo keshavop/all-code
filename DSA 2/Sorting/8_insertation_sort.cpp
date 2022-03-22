2c6cf6f047bc5d2ee1643352965784a52b9a3050
Keshav Kumar <keshavkumar8122@gmail.com>
2022-03-22 17:33:44 +0530

    sorting

--- DSA 2/Sorting/8_insertation_sort.cpp
+++ DSA 2/Sorting/8_insertation_sort.cpp
@@ -1,13 +1,27 @@
 // Insertation sort
-//
+// find a smallest element compare it to all previous elements and shift all elements 1 place next and placed the smallest element in desired place.
 
-// Time -> 
-// Space - > 
+// Time -> 0(n^2)
+// Space - > 0(n)
 
 #include <iostream>
 using namespace std;
 
-void Insertat
+void InsertationSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int current = arr[i];
+
+        int prev = i - 1;
+        while (prev >= 0 && arr[prev] > current)
+        {
+            arr[prev + 1] = arr[prev];
+            prev = prev - 1;
+        }
+        arr[prev + 1] = current;
+    }
+}
 
 int main()
 {

b53978f5469da054f86935566173b95d86deb654
Keshav Kumar <keshavkumar8122@gmail.com>
2022-03-26 18:47:23 +0530

    cpp

--- C++/star_patterm.cpp
+++ C++/star_patterm.cpp
@@ -12,11 +12,18 @@ using namespace std;
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     int i = 1;
-    while (i<=n)
+    while (i <= n)
     {
-        cout<<"*";
+        int stars = 1;
+        while (stars <= i)
+        {
+
+            cout << "*";
+            stars++;
+        }
+        cout << endl;
         i++;
     }
     return 0;

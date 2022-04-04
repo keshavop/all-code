d2e502e772149ab25ef5c7c6d97dcd7b231e5650
Keshav Kumar <keshavkumar8122@gmail.com>
2022-04-04 23:09:57 +0530

    array

---
+++ C++/find_missing_number.cpp
@@ -0,0 +1,34 @@
+/*
+find Missing number.
+Given an array A of size (n-1) with all integers from 1 to n except one missing number.
+
+
+Implement a function that takes A as an argument and returns the missing number.
+
+
+Sample Input
+
+3 4 2
+
+6 3 2 5 1
+
+Sample Output
+
+1
+
+4
+
+Explanation :
+
+for test case one, n = 4 and 1 is missing.
+
+Similarly for test case two, n = 6 and 4 is missing.
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    
+    return 0;
+}

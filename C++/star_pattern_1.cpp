1b115f95f92806faa295f37b36ef5c58e99e57ad
Keshav Kumar <keshavkumar8122@gmail.com>
2022-05-05 00:02:50 +0530

    c++

--- C++/star_pattern_1.cpp
+++ C++/star_pattern_1.cpp
@@ -1,8 +1,10 @@
-/* star pattern
+/*
+star pattern
 
-*
-**
-***
+    *
+   ***
+  *****
+ *******
 
 */
 
@@ -11,6 +13,20 @@ using namespace std;
 
 int pattern(int n)
 {
+    for (int i = 1; i < n; i++)
+    {
+        // spaces
+        for (int spaces = 1; spaces <= n - i; spaces++)
+        {
+            cout << " ";
+        }
+        // stars
+        for (int stars = 1; stars <= 2 * i - 1; stars++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
 }
 
 int main()

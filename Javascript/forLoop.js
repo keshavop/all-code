4ce9cad7ecb7ca3fdafb1ffa4327874e140facb4
Keshav Kumar <keshavkumar8122@gmail.com>
2022-05-28 00:27:37 +0530

    forloop js

---
+++ Javascript/forLoop.js
@@ -0,0 +1,19 @@
+// for loop
+for(let i=1;i<=10;i++){
+    console.log(i);
+}
+
+// printing even no
+for (let index = 2; index < 20; index=index+2) {
+    console.log(index);
+}
+
+// printing odd no
+for (let index = 1; index < 20; index=index+2) {
+    console.log(index);
+}
+
+// printing from 100 to 10
+for (let index = 100; index >= 10; index=index-10) {
+    console.log(index);
+}

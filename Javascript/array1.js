13d41075cb3b95325dd30e3bffafac3dccd5841d
Keshav Kumar <keshavkumar8122@gmail.com>
2022-05-27 21:42:23 +0530

    array js

---
+++ Javascript/array1.js
@@ -0,0 +1,26 @@
+let week=['Monday','Tuesday','Wednesday','Thrusday','Friday','Saturday']
+
+// printing 1st element of array
+console.log(week[0]);
+
+// printing all elements
+for (let index = 0; index < week.length; index++) {
+    console.log(week[index]);
+}
+
+// insert at end of array
+week.push('Sunday','sunday2');
+
+// pop the last element from end
+week.pop()
+
+// 
+week.sort()
+
+// 2D ARRAY
+const gameBoard=[['O','O','O'],['O','O','X'],['O','X','X']];
+
+// printing 2d array
+for (let index = 0; index < 1; index++) {
+    console.log(gameBoard);
+}

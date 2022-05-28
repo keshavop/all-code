a015201499eaf015ac8d0508461af9dcaf6aeba6
Keshav Kumar <keshavkumar8122@gmail.com>
2022-05-28 19:06:58 +0530

    function

---
+++ Javascript/function.js
@@ -0,0 +1,30 @@
+// definitation
+function fun1() {
+    console.log("kiki");
+}
+
+// function call
+fun1();
+
+
+// function call with arguments
+function fun2(name){
+    console.log(name);
+}
+
+fun2('keshav');
+
+
+// sum of 2 no.
+function sum(n1,n2) {
+    console.log(n1+n2);
+}
+
+sum(8,7);
+
+// sum of 3 no.
+function sum1(n1,n2,n3) {
+    console.log(n1+n2+n3);
+}
+
+sum1(8,7,6);

64aa8325ae8cad46e592a4efa37e4bdf5f255bb8
Keshav Kumar <keshavkumar8122@gmail.com>
2022-05-29 22:58:07 +0530

    map js

--- Javascript/forEachLoop.js
+++ Javascript/forEachLoop.js
@@ -1,5 +1,6 @@
 const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// for each loop
 nums.forEach(element => {
     console.log(element);
 });
---
+++ Javascript/map.js
@@ -0,0 +1,7 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const double=nums.map(function (ele){
+    return ele *2
+})
+
+console.log(double);

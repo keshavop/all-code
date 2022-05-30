107992cdc6c9d0a1dd762935b4e73083d3dbcfa3
Keshav Kumar <keshavkumar8122@gmail.com>
2022-05-30 22:51:18 +0530

    set timeout and set interval

---
+++ Javascript/setTimeout_setInterval.js
@@ -0,0 +1,12 @@
+console.log("hello");
+
+// setTimeout print after approx 3 seconds.
+setTimeout(() => {
+    console.log("....... waiting hmm");
+}, 3000);
+
+
+// setInterval print after a certain duration
+setInterval(() => {
+    console.log(Math.random());
+}, 2000);

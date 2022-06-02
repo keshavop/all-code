1293eea1ba7265f09be875d1974d376de0c582c1
Keshav Kumar <keshavkumar8122@gmail.com>
2022-06-02 23:17:08 +0530

    dsa

--- DSA 2/array-CM/12_largest_subarray_sum_kadane_algorithm.cpp
+++ DSA 2/array-CM/12_largest_subarray_sum_kadane_algorithm.cpp
@@ -3,12 +3,13 @@
 // Space - 0(1)
 
 #include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int largest_subarray_sum3(int arr[], int n)
 {
     int cs = 0;
-    int largest_sum = 0;
+    int largest_sum = INT_MIN;
     for (int i = 0; i < n; i++)
     {
         cs = cs + arr[i];
@@ -23,7 +24,7 @@ int largest_subarray_sum3(int arr[], int n)
 
 int main()
 {
-    int arr[] = {1, 2, 3, -2, 5};
+    int arr[] = {-1, -2, -3, -4};
     int n = sizeof(arr) / sizeof(int);
     cout << largest_subarray_sum3(arr, n);
     return 0;

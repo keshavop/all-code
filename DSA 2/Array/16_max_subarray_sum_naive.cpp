a41968b9978230df1b479ee3b9ed0b807065f70d
Keshav Kumar <keshavkumar8122@gmail.com>
2022-06-15 21:14:31 +0530

    dsa

---
+++ DSA 2/Array/16_max_subarray_sum_naive.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int sum(int arr[], int n){
+
+}
+
+int main()
+{
+    int arr[]={};
+    int n = sizeof(arr[0])/sizeof(int);
+    sum(arr,n);
+    return 0;
+}

1ce0126887113b5c243f8b5c1070662aafe5ba94
Keshav Kumar <keshavkumar8122@gmail.com>
2022-06-17 11:04:47 +0530

    Create majority_element.cpp

---
+++ DSA 2/Array/majority_element.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int majority(int arr[],int n){
+    
+}
+
+int main()
+{
+    int n = 6;
+    int arr[]={1,2,3,5,5,6};
+    majority(arr,n);
+    return 0;
+}

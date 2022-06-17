4bc29b858a294725997f10c8791c17e5b1d7dfd7
Keshav Kumar <keshavkumar8122@gmail.com>
2022-06-18 01:23:55 +0530

    majority element
    testcase failed

--- DSA 2/Array/majority_element.cpp
+++ DSA 2/Array/majority_element.cpp
@@ -1,26 +1,32 @@
 #include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int majority(int arr[], int n)
 {
-    int count = 0;
     for (int i = 0; i < n; i++)
     {
+        int count = 1;
         for (int j = 1; j < n; j++)
         {
             if (arr[i] == arr[j])
             {
                 count++;
             }
+            if (count >= n / 2)
+            {
+                cout << "element is " << n << endl;
+                return i;
+            }
         }
     }
-    return count;
+    return -1;
 }
 
 int main()
 {
     int n = 6;
-    int arr[] = {1, 2, 3, 5, 5, 6};
-    cout<<majority(arr, n);
+    int arr[] = {2,1,1,1,2,2};
+    cout << majority(arr, n);
     return 0;
 }

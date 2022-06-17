605738768477ca9db2360f3f1e46944919dc3aac
Keshav Kumar <keshavkumar8122@gmail.com>
2022-06-17 19:18:14 +0530

    array

--- DSA 2/Array/majority_element.cpp
+++ DSA 2/Array/majority_element.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int majority(int arr[],int n){
-    
+int majority(int arr[], int n)
+{
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 1; j < n; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+            }
+        }
+    }
+    return count;
 }
 
 int main()
 {
     int n = 6;
-    int arr[]={1,2,3,5,5,6};
-    majority(arr,n);
+    int arr[] = {1, 2, 3, 5, 5, 6};
+    cout<<majority(arr, n);
     return 0;
 }

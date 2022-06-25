9380098111e054075b4774b4e352a8f74c949962
Keshav Kumar <keshavkumar8122@gmail.com>
2022-06-25 16:42:07 +0530

    peak element in an array

--- DSA 2/Searching/10_peak_element_in_an_array_eff.cpp
+++ DSA 2/Searching/10_peak_element_in_an_array_eff.cpp
@@ -1,4 +1,5 @@
 // peak element in an array eff approach
+// peak element are those which is greader than left as well as right element
 
 // Time -> O(N)
 // Space -> O(1)
@@ -11,19 +12,29 @@ int peakElement(vector<int> nums)
 {
     int n = nums.size();
     int low = 0;
-    int high = n-1;
-    while (low<high)
+    int high = n - 1;
+    while (low < high)
     {
-        int mid = (low + high)/2;
-        // if(mid ==0 && [mid-1])
+        int mid = (low + high) / 2;
+        if (mid == 0 || nums[mid - 1] <= nums[mid] && mid == n - 1 || nums[mid + 1] <= nums[mid])
+        {
+            return mid;
+        }
+        else if (mid > 0 && nums[mid - 1] >= nums[mid])
+        {
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
     }
-    
     return -1;
 }
 
 int main()
 {
-    vector<int> arr{1,2,1,3,5,6,4};
-    cout<<peakElement(arr);
+    vector<int> arr{1,2};
+    cout << peakElement(arr);
     return 0;
 }

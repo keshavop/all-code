f5d60eb2c5d0e86b5c38f94e0d93a3cb103121e5
Keshav Kumar <keshavkumar8122@gmail.com>
2022-07-01 22:20:55 +0530

    linked list

---
+++ DSA 2/Linked List/01_linkedList_traversal.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node(int d)
+    {
+        data = d;
+        next = NULL;
+    }
+};
+
+void print(node *head)
+{
+    while (head != NULL)
+    {
+        cout << head->data << " ";
+        head = head->next;
+    }
+}
+int main()
+{
+    node *head = new node(10);
+    head->next = new node(20);
+    head->next->next = new node(30);
+    head->next->next->next = new node(40);
+    print(head);
+    return 0;
+}

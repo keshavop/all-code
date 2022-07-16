5a1ec7d09a328aab7fb2fe70b514893b01ef46f2
Keshav Kumar <keshavkumar8122@gmail.com>
2022-07-16 22:27:18 +0530

    Update 6_check_for_anagram_eff.cpp

--- DSA 2/String/6_check_for_anagram_eff.cpp
+++ DSA 2/String/6_check_for_anagram_eff.cpp
@@ -10,18 +10,18 @@ using namespace std;
 const int CHAR = 256;
 bool isAnagram(string s1, string s2)
 {
-    if (s1.size() != s2.size())
+    if (s1.size() != s2.size())  //if size is not equal then false
     {
         return false;
     }
     int count[CHAR] = {0};
-    for (int i = 0; i < s1.length(); i++)
+    for (int i = 0; i < s1.length(); i++)   
     {
         count[s1[i]++];
         count[s2[i]--];
     }
 
-    for (int i = 0; i < CHAR; i++)
+    for (int i = 0; i < CHAR; i++)  // if char array is not empty then false
     {
         if (count[i] != 0)
         {

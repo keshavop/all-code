a4c8cd6ed0277fac828082160a1eaa6f533ce859
Keshav Kumar <keshavkumar8122@gmail.com>
2022-07-18 21:43:42 +0530

    string day 4

--- DSA 2/String/7_leftmost_repeating_element_eff.cpp
+++ DSA 2/String/7_leftmost_repeating_element_eff.cpp
@@ -7,14 +7,28 @@
 #include <iostream>
 using namespace std;
 
-int leftmost(string s){
+const int CHAR = 256;
 
+int leftmost(string s)
+{
+    bool visited[CHAR];
+    fill(visited, visited + CHAR, false);
+
+    int res = -1;
+    for (int i = s.length() - 1; i >= 0; i--)
+    {
+        if (visited[s[i]])
+            res = i;
+        else
+            visited[s[i]] = true;
+    }
+    return res;
 }
 
 int main()
 {
     string s = "abbcc";
 
-    leftmost(s);
+    cout << leftmost(s);
     return 0;
 }

0675281d67bb71236bd86dbb87d6d361892454ad
Keshav Kumar <keshavkumar8122@gmail.com>
2022-08-08 15:04:34 +0530

    Update two_sum.cpp

--- DSA 2/Random DSA Ques/two_sum.cpp
+++ DSA 2/Random DSA Ques/two_sum.cpp
@@ -2,6 +2,25 @@
 #include <iostream>
 using namespace std;
 
+
+// class Solution
+// {
+// public:
+//     vector<int> twoSum(vector<int> &nums, int target)
+//     {
+//         unordered_map<int, int> seen;
+//         for (int i = 0; i < nums.size(); ++i)
+//         {
+//             int b = nums[i], a = target - b;
+//             if (seen.count(a))
+//                 return {seen[a], i}; // Found pair of (a, b), so that a + b = target
+//             seen[b] = i;
+//         }
+//         return {};
+//     }
+// };
+
+
 vector<int> twoSum(vector<int> &nums, int target)
 {
     vector<int> ans;

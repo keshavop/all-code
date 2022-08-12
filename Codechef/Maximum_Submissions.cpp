b606c57c093e359faf871a4685875a2c24d1bbb1
Keshav Kumar <keshavkumar8122@gmail.com>
2022-08-12 17:54:44 +0530

    codechef long challenge

---
+++ Codechef/Maximum_Submissions.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int min;
+        cin>>min;
+        int sub = min*2;
+        cout<<sub<<endl;
+    }
+
+    return 0;
+}

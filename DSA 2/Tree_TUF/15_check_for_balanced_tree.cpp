d60d9a37a037bc096df33f8de785c79596b4ac12
Keshav Kumar <keshavkumar8122@gmail.com>
2022-08-22 23:14:06 +0530

    tree day 3

--- DSA 2/Tree_TUF/15_check_for_balanced_tree.cpp
+++ DSA 2/Tree_TUF/15_check_for_balanced_tree.cpp
@@ -27,15 +27,8 @@ int dfsHeight(node *root)
         return 0;
     }
     int leftHeight = dfsHeight(root->left);
-    if (leftHeight = -1)
-    {
-        return -1;
-    }
     int rightHeight = dfsHeight(root->right);
-    if (rightHeight = -1)
-    {
-        return -1;
-    }
+
     if (abs(leftHeight - rightHeight) > 1)
     {
         return -1;
--- DSA 2/Tree_TUF/16_diameter_of_a_binary_tree.cpp
+++ DSA 2/Tree_TUF/16_diameter_of_a_binary_tree.cpp
@@ -1,3 +1,5 @@
+// not working but works on leetcode
+
 // Diameter of a binary tree
 
 // Time -> ()
@@ -7,27 +9,48 @@
 #include <iostream>
 using namespace std;
 
-class node{
+class TreeNode
+{
 public:
-    int data;
-    node *left;
-    node *right;
-    node(int val)
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val)
     {
-        data = val;
+        val = val;
         left = right = NULL;
     }
 };
 
+int height(TreeNode *root, int diameter)
+{
+    if (root == NULL)
+    {
+        return 0;
+    }
+    int lh = height(root->left, diameter);
+    int rh = height(root->right, diameter);
+    diameter = max(diameter, lh + rh);
+
+    return 1 + max(lh, rh);
+}
+
+int diameterOfBinaryTree(TreeNode *root)
+{
+    int diameter = 0;
+    height(root, diameter);
+    return diameter;
+}
+
 int main()
 {
-    node *root = new node(10);
-    root->left = new node(20);
-    root->right = new node(30);
-    root->right->left = new node(40);
-    root->right->right = new node(50);
-    root->right->right->right = new node(80);
+    TreeNode *root = new TreeNode(10);
+    root->left = new TreeNode(20);
+    root->right = new TreeNode(30);
+    root->right->left = new TreeNode(40);
+    root->right->right = new TreeNode(50);
+    root->right->right->right = new TreeNode(80);
 
-    
+    cout<<diameterOfBinaryTree(root);
     return 0;
 }

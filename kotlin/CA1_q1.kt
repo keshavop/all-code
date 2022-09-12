ad9e435da322e4cc00eadd97727c03eefcec624d
Keshav Kumar <keshavkumar8122@gmail.com>
2022-09-12 23:26:43 +0530

    kt CA

---
+++ kotlin/CA1_q1.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val cities = arrayOf("Hyderabad","Chennai","Mumbai","Kolkata","Bengaluru");
+    
+    if(countAt[cities[i]]==2){
+        println(cities)
+    }
+}
---
+++ kotlin/CA1_q2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val str:String = "CSE";
+
+    println("*****  !!!!! ^^^^^")
+    println("*      !     ^")
+    println("*      !     ^")
+    println("*      !!!!! ^^^^^")
+    println("*          ! ^")
+    println("*          ! ^")
+    println("*****  !!!!! ^^^^^")
+
+}
